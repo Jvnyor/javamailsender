170d793739db744ef2c67ef19b7fd06c428741ea
Josias Silva <josias.silva@nagem.com.br>
2021-10-14 15:50:04 -0300

    first commit

---
+++ src/test/java/com/josias/javamailsender/JavamailsenderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.josias.javamailsender;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavamailsenderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
